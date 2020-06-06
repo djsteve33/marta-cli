b90b0e983a2d483d951ecfac569c9eeeeee9e12e
Steve Womble <swomble@aol.com>
2020-06-06 19:44:19 +0000

    updated methods in Marta class

--- lib/marta_cli/marta.rb
+++ lib/marta_cli/marta.rb
@@ -1,3 +1,15 @@
 class Marta
-  puts "Marta class loaded"
+  
+  attr_reader :station_name, :destination_name
+  @@all = []
+  def initialize(station_name, destination_name)
+    @station_name = station_name
+    @destination_name = destination_name
+    @@all << self 
+  end
+  
+  def self.all
+    @@all
+  end 
+  
 end 
