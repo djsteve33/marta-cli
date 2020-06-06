b019fbba4917473c3087fc4beec0c1dd32af933a
Steve Womble <swomble@aol.com>
2020-06-06 19:38:45 +0000

    updated get method in Api class

--- lib/marta_cli/api.rb
+++ lib/marta_cli/api.rb
@@ -1,9 +1,14 @@
 class Api 
-  BASE_URL = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?'
+  BASE_URL = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?apikey=18eb4629-edaa-43ea-aa34-1848d012c788'
   
   def self.get_marta  #Contacts API URL to pull data
     res = RestClient.get(BASE_URL)
-    binding.pry
+    data = JSON.parse(res.body)
+    data['results'].each do |marta|
+      station = marta['station']
+      destination = marta['destination']
+      Marta.new(station, destination)
+    end 
   end
   
 end
