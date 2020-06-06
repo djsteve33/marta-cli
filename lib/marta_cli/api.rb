b0bde67f58c38cbef72a3973ce393c1b3a1217af
Steve Womble <swomble@aol.com>
2020-06-06 19:20:00 +0000

    added get method to Api class

--- lib/marta_cli/api.rb
+++ lib/marta_cli/api.rb
@@ -1,3 +1,9 @@
 class Api 
-  puts "Api class loaded"
+  BASE_URL = 'http://developer.itsmarta.com/RealtimeTrain/RestServiceNextTrain/GetRealtimeArrivals?'
+  
+  def self.get_marta  #Contacts API URL to pull data
+    res = RestClient.get(BASE_URL)
+    binding.pry
+  end
+  
 end
