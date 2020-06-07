4055e67b387db07d44700e4506fe3b48e09904e9
Steve Womble <swomble@aol.com>
2020-06-07 17:25:41 +0000

    adding/updating some methods to Cli class

--- lib/marta_cli/cli.rb
+++ lib/marta_cli/cli.rb
@@ -8,14 +8,24 @@ class Cli
    
    def main
      print_all
+     print_station_prompt
+     station = prompt_selection
    end
    
    def print_all
      Marta.all.each {|m| puts "#{m.station}"}
    end
    
+   def print_station_prompt
+     puts "For more train info, please select the station that you will be departing from:"
+   end
+   
+   def prompt_selection
+     gets.chomp
+   end
+   
    def welcome
-     puts Rainbow("Welcome to Marta train info!").blue
+     puts Rainbow("Welcome to Marta Train Info!").blue.bright
    end 
    
    
